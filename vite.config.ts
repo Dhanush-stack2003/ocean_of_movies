12e7c0e795d288f199011a358898d02a5cdaabe2
Dhanushvg <dhanushv247@gmail.com>
2025-07-23 13:05:36 +0530

    add vite_base_path

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test:{
-    environment:'jsdom',
-    globals:true,
-    setupFiles:'./src/setupTest.ts'
-  }
-})
+  base: process.env.VITE_BASE_PATH || "/ocean_of_movies/tree/main",
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTest.ts",
+  },
+});
